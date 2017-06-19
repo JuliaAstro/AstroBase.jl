2d7ff8aeb2dc71a70c7ed5e6f136d17215c62054
Helge Eichhorn <git@helgeeichhorn.de>
2017-06-19 21:50:29 +0200

    Add epoch stub

--- src/basetypes.jl
+++ src/basetypes.jl
@@ -1,5 +1,5 @@
 export AbstractState
-export Frame, GCRF
+export Frame, GCRF, epoch
 
 abstract type AbstractState end
 
@@ -8,3 +8,5 @@ abstract type Frame end
 struct GCRF <: Frame end
 
 Base.show(io::IO, ::Type{F}) where F<:Frame = print(io, Base.datatype_name(F))
+
+function epoch() end
