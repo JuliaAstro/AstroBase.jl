437dcc05edb71408d6b451106b5dd894a786317f
Helge Eichhorn <git@helgeeichhorn.de>
2017-06-20 17:14:59 +0200

    Add isrotating function

--- src/basetypes.jl
+++ src/basetypes.jl
@@ -1,9 +1,11 @@
 export AbstractState
-export Frame, GCRF, epoch
+export Frame, GCRF, epoch, isrotating
 
 abstract type AbstractState end
 
 abstract type Frame end
+
+isrotating(::Type{F}) where {F<:Frame} = false
 
 struct GCRF <: Frame end
 
