a81be993345b925ff768d3487d2f4334c99a7cea
Helge Eichhorn <git@helgeeichhorn.de>
2017-11-18 11:37:27 +0100

    Work around Win32 test failure

--- test/states.jl
+++ test/states.jl
@@ -34,7 +34,7 @@ load_ephemeris!(SPK, path(de430))
 
     @test iss == State(ep, r, v, GCRF, Earth)
     @test iss ≈ State(ep, r + eps(), v + eps(), GCRF, Earth)
-    @test State(iss, frame=IAUEarth) == State(ep, rot(r, v)..., IAUEarth)
+    @test State(iss, frame=IAUEarth) ≈ State(ep, rot(r, v)..., IAUEarth)
     @test iss ≈ State(State(iss, frame=IAUEarth), frame=GCRF)
     @test State(iss, timescale=TDB) == State(tdb, r, v)
     @test State(iss, body=MarsBarycenter) ==
@@ -43,7 +43,7 @@ load_ephemeris!(SPK, path(de430))
         State(tdb, r_mars, v_mars, GCRF, MarsBarycenter)
     @test State(iss, frame=IAUEarth, body=MarsBarycenter) ≈
         State(ep, rot(r_mars, v_mars)..., IAUEarth, MarsBarycenter)
-    @test State(iss, frame=IAUEarth, timescale=TDB) ==
+    @test State(iss, frame=IAUEarth, timescale=TDB) ≈
         State(tdb, rot(r, v)..., IAUEarth)
     @test State(State(mars_rot, body=Earth), body=MarsBarycenter) ≈ mars_rot
     @test State(State(mars_rot, frame=ITRF, body=Earth), frame=IAUMars,
