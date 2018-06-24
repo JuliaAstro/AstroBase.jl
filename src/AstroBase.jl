9e6cc72256a440d0b05f1fbc6d906c2d1a616b14
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-06-24 16:06:07 +0530

    a00

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -1,5 +1,22 @@
 module AstroBase
 
-# package code goes here
+export a00, anp
+const J2000 = 2451545.0
+anp = x -> mod(x, 2pi) < 0? mod(x, 2pi)+2pi:mod(x, 2pi)
 
-end # module
+function a00(jd1, jd2)
+    if jd1 < jd2
+        d1 = jd1
+        d2 = jd2
+    else
+        d1 = jd2
+        d2 = jd1
+    end
+     t = d1 + d2 - J2000
+
+     f = mod(d1, 1.0) + mod(d2, 1.0)
+
+     anp(2pi * (f + 0.7790572732640 + 0.00273781191135448 * t))
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using AstroBase
+using ERFA
 using Base.Test
 
+
 # write your own tests here
-@test 1 == 2
+@test AstroBase.anp(-10) == ERFA.anp(-10)
+@test AstroBase.anp(10) == ERFA.anp(10)
+@test AstroBase.a00(2.4578265e6, 0.30434616919175345) ≈ ERFA.era00(2.4578265e6, 0.30434616919175345)
