42de9153b9b8b25de01dce759b959649ab174162
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-06-25 11:57:21 +0530

    sp00

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -1,5 +1,14 @@
 module AstroBase
 
+export sp00
+const J000 = 2451545.0
+const DAYS_PER_CENTURY = 36525.0
+const ARCSEC_TO_RAD = 4.848136811095359935899141e-6
 # package code goes here
+function sp00(jd1, jd2)
+    t = (jd1 - J000 + jd2) / DAYS_PER_CENTURY
+    sp = -47e-6 * t * ARCSEC_TO_RAD
+    sp
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using AstroBase
+using ERFA
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@test AstroBase.sp00(2.4578265e6, 0.30434616919175345) == ERFA.sp00(2.4578265e6, 0.30434616919175345)
