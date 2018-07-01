1fd4cc717fa3e77e9696f882f4c3f25afe8e6eca
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-07-01 13:44:16 +0530

    docs added

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -1,14 +1,25 @@
 module AstroBase
 
-export sp00
-const J000 = 2451545.0
+export tio_locator
+const J2000 = 2451545.0
 const DAYS_PER_CENTURY = 36525.0
 const ARCSEC_TO_RAD = 4.848136811095359935899141e-6
 # package code goes here
-function sp00(jd1, jd2)
-    t = (jd1 - J000 + jd2) / DAYS_PER_CENTURY
-    sp = -47e-6 * t * ARCSEC_TO_RAD
-    sp
+"""
+    tio_locator(jd1, jd2)
+
+The TIO locator s', positioning the Terrestrial Intermediate Origin
+on the equator of the Celestial Intermediate Pole.
+
+'''
+jldoctest
+julia> AstroBase.tio_locator(2.4578265e6, 0.30434616919175345)
+-3.9189245827947945e-11
+'''
+"""
+function tio_locator(jd1, jd2)
+    t = (jd1 - J2000 + jd2) / DAYS_PER_CENTURY
+    -47e-6 * t * ARCSEC_TO_RAD
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,4 @@ using ERFA
 using Base.Test
 
 # write your own tests here
-@test AstroBase.sp00(2.4578265e6, 0.30434616919175345) == ERFA.sp00(2.4578265e6, 0.30434616919175345)
+@test AstroBase.tio_locator(2.4578265e6, 0.30434616919175345) == ERFA.sp00(2.4578265e6, 0.30434616919175345)
