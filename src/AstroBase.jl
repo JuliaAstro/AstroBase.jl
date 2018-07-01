edd935f7cde2f115b80f58cb2764118ac1f3a33f
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-07-01 13:35:46 +0530

    docs

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -1,8 +1,22 @@
 module AstroBase
 
 using Rotations
-export c2ixys
+export celestial_to_intermediate
 # package code goes here
+"""
+    celestial_to_intermediate(x, y, s)
+
+Converts the celestial to intermediate-frame-of-date matrix given the CIP
+X,Y and the CIO locator s.
+
+```jldoctest
+julia> celestial_to_intermediate(0.2, 0.2, 0.1)
+3×3 RotZYZ{Float64}(0.785398, 0.286757, -0.885398):
+  0.976728   0.0774803  0.2
+ -0.11811    0.972651   0.2
+ -0.179034  -0.218968   0.959166
+```
+"""
 function celestial_to_intermediate(x, y, s)
     r2 = x^2+y^2
     e = r2 > 0.0?atan2(y,x):0.0
