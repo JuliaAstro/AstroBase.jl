0d0efa40431275496e602a4f66b93d2972cc914e
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-07-01 13:57:48 +0530

    docs added

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -1,8 +1,22 @@
 module AstroBase
 
 using Rotations
-export pom00
-@inline function pom00(rx, ry, sp)
+export polar_motion
+
+"""
+    polar_motion(rx, ry, sp)
+
+Form the matrix of polar motion for coordinates of the pole (radians).
+```
+jldoctest
+julia> polar_motion(20, 30, 50)
+3×3 RotZYX{Int64}(50, -20, -30):
+  0.393785  -0.829946  -0.395124
+ -0.10707    0.385514  -0.916469
+  0.912945   0.403198   0.0629472
+```
+"""
+function polar_motion(rx, ry, sp)
     RotZYX(sp, -rx, -ry)
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,5 @@ using ERFA
 using Base.Test
 
 # write your own tests here
-@test AstroBase.pom00(30, 30, 30) == ERFA.pom00(30, 30, 30)
-@test AstroBase.pom00(20, 30, 50) == ERFA.pom00(20, 30, 50)
+@test AstroBase.polar_motion(30, 30, 30) == ERFA.pom00(30, 30, 30)
+@test AstroBase.polar_motion(20, 30, 50) == ERFA.pom00(20, 30, 50)
