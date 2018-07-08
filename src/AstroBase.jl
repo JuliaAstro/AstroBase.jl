0193470cf41374a2fc405f4e7689134d64c987ea
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-07-08 14:36:50 +0530

    changes

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -11,14 +11,14 @@ Form the matrix of polar motion for coordinates of the pole (radians).
 # Example
 ```jldoctest
 julia> polar_motion(20, 30, 50)
-3×3 RotZYX{Int64}(50, -20, -30):
+3×3 RotZYX{Float64}(50.0, -20.0, -30.0):
   0.393785  -0.829946  -0.395124
  -0.10707    0.385514  -0.916469
   0.912945   0.403198   0.0629472
 ```
 """
 function polar_motion(rx, ry, sp)
-    RotZYX(sp, -rx, -ry)
+    RotZYX{Float64}(sp, -rx, -ry)
 end
 
 end
