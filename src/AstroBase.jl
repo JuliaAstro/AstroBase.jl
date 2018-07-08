2265a424a60db2e5a4590ddd7fce761833dd8075
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-07-08 14:14:47 +0530

    changes

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -1,18 +1,44 @@
 module AstroBase
 
-export tio_locator
+export tio_locator, sec2rad, rad2sec
 const J2000 = 2451545.0
 const DAYS_PER_CENTURY = 36525.0
-sec2rad(sec::Real) = deg2rad(sec / 3600)
 # package code goes here
+"""
+    sec2rad(sec::Real)
+
+Returns radians for given seconds.
+
+# Example
+
+```jldoctest
+julia> sec2rad(3600 * 30)
+0.5235987755982988
+```
+"""
+sec2rad(sec::Real) = deg2rad(sec / 3600)
+
+"""
+    rad2sec(rad::Real)
+Returns angle (radians) for given seconds.
+
+# Example
+
+```jldoctest
+julia> rad2sec(0.5235987755982988)
+107999.99999999999
+```
+"""
+rad2sec(rad::Real) = rad2deg(rad) * 3600
+
 """
     tio_locator(jd1, jd2)
 
-The TIO locator s', positioning the Terrestrial Intermediate Origin
-on the equator of the Celestial Intermediate Pole.
+Returns TIO locator s' position for a given 2-part Julian date (jd1, jd2).
 
-'''
-jldoctest
+# Example
+
+'''jldoctest
 julia> AstroBase.tio_locator(2.4578265e6, 0.30434616919175345)
 -3.9189245827947945e-11
 '''
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,5 @@ using Base.Test
 
 # write your own tests here
 @test AstroBase.tio_locator(2.4578265e6, 0.30434616919175345) == ERFA.sp00(2.4578265e6, 0.30434616919175345)
+@test AstroBase.sec2rad(3600) == deg2rad(1)
+@test AstroBase.rad2sec(1) == rad2deg(1) * 3600
