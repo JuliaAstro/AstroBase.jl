bbda96109f53887bc180f6826909e0d046724a47
Helge Eichhorn <git@helgeeichhorn.de>
2018-07-08 17:27:55 +0200

    Use approximate equality

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,6 @@ using Base.Test
     @test AstroBase.earth_rotation_angle(2.4578265e6, 0.30434616919175345) ≈ ERFA.era00(2.4578265e6, 0.30434616919175345)
     
     # Polar motion
-    @test AstroBase.polar_motion(30, 30, 30) == ERFA.pom00(30, 30, 30)
-    @test AstroBase.polar_motion(20, 30, 50) == ERFA.pom00(20, 30, 50)
+    @test AstroBase.polar_motion(30, 30, 30) ≈ ERFA.pom00(30, 30, 30)
+    @test AstroBase.polar_motion(20, 30, 50) ≈ ERFA.pom00(20, 30, 50)
 end
