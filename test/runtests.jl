9a1b0df3c21cf1f6ba24e8b0fcc69b7ce3606795
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-07-12 16:05:10 +0530

    sp00 (#5)
    * sp00
    
    * docs added
    
    * correction
    
    * changes
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update AstroBase.jl
    
    * Update AstroBase.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,15 @@ using Base.Test
 @testset "IERS" begin
     # Earth rotation angle
     @test AstroBase.earth_rotation_angle(2.4578265e6, 0.30434616919175345) ≈ ERFA.era00(2.4578265e6, 0.30434616919175345)
-    
+
     # Polar motion
     @test AstroBase.polar_motion(30, 30, 30) ≈ ERFA.pom00(30, 30, 30)
     @test AstroBase.polar_motion(20, 30, 50) ≈ ERFA.pom00(20, 30, 50)
+    
+    @test AstroBase.tio_locator(2.4578265e6, 0.30434616919175345) == ERFA.sp00(2.4578265e6, 0.30434616919175345)
+    
+    # Radians <-> arcseconds
+    @test AstroBase.sec2rad(3600) == deg2rad(1)
+    @test AstroBase.rad2sec(1) == rad2deg(1) * 3600
 end
+
