eff3af6c69536556f891bb986499e3dfe4d40ee3
Helge Eichhorn <git@helgeeichhorn.de>
2018-07-12 12:32:37 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using AstroBase
 using ERFA
 using Base.Test
 
-# write your own tests here
 @testset "IERS" begin
     # Earth rotation angle
     @test AstroBase.earth_rotation_angle(2.4578265e6, 0.30434616919175345) ≈ ERFA.era00(2.4578265e6, 0.30434616919175345)
