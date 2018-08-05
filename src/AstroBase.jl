c0b7e795cc835ff451c84914e3abe9f7380576a4
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-08-05 15:00:35 +0530

    Conversions (#28)
    * added nutm80
    
    * ported all
    
    * reorganized
    
    * reorganized
    
    * static test cases
    
    * static test cases
    
    * docs

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -1,6 +1,7 @@
 module AstroBase
 
 using Reexport
+using Rotations
 
 include("util.jl")
 include(joinpath("bodies", "Bodies.jl"))
@@ -8,5 +9,6 @@ include("EarthAttitude.jl")
 
 @reexport using .Bodies
 @reexport using .EarthAttitude
+
 
 end # module
