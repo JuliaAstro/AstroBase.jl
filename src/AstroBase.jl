8c4b50978cff6a6eab0142819792cccac4695bc3
Helge Eichhorn <git@helgeeichhorn.de>
2018-09-25 09:40:34 +0200

    Add generic ephemeris interface

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -3,12 +3,14 @@ module AstroBase
 using Reexport
 using Rotations
 
+@reexport using AstroTime
+
 include("util.jl")
 include(joinpath("bodies", "Bodies.jl"))
 include("EarthAttitude.jl")
+include("ephemerides.jl")
 
 @reexport using .Bodies
 @reexport using .EarthAttitude
-
 
 end # module
