87698933239064125e3766d4bd69c335c289a4db
Helge Eichhorn <git@helgeeichhorn.de>
2019-02-06 11:39:53 +0100

    Rewrite orbit range test to use multiprocessing

--- test/orbit_ranges.jl
+++ test/orbit_ranges.jl
@@ -1,9 +1,64 @@
-using AstroBase
 using Base.Iterators: product
-using Base.Threads: nthreads, @threads
+using Distributed
+
+@everywhere using AstroBase
 
 μ = 3.986004418e14
-n = 2
+
+procs = nprocs()
+n = 4
+abort = false
+
+if !isempty(ARGS)
+    try
+        global n
+        n = something(tryparse.(Int, ARGS)...)
+    catch
+        # pass
+    end
+
+    abort = "abort" in ARGS
+else
+    if procs > 1
+        n = (procs - 1) * 2
+    end
+end
+
+@everywhere function keplerian_roundtrip(ele₀, μ; abort=false)
+    ele₁ = keplerian(cartesian(ele₀..., μ)..., μ)
+    failed = !(isapprox(ele₁[1], ele₀[1], rtol=1.0) &&
+               isapprox(ele₁[2], ele₀[2], atol=sqrt(eps())) &&
+               isapprox(ele₁[3], ele₀[3], atol=sqrt(eps())) &&
+               isapprox(ele₁[4], ele₀[4], atol=sqrt(eps())) &&
+               isapprox(ele₁[5], ele₀[5], atol=sqrt(eps())) &&
+               isapprox(abs(ele₁[6]), abs(ele₀[6]), atol=sqrt(eps())))
+    if failed && abort
+        error("Results do not match!\n",
+              "a₁: ", rpad(ele₁[1], 24), "a₀:", ele₀[1], "\n",
+              "e₁: ", rpad(ele₁[2], 24), "e₀:", ele₀[2], "\n",
+              "i₁: ", rpad(ele₁[3], 24), "i₀:", ele₀[3], "\n",
+              "Ω₁: ", rpad(ele₁[4], 24), "Ω₀:", ele₀[4], "\n",
+              "ω₁: ", rpad(ele₁[5], 24), "ω₀:", ele₀[5], "\n",
+              "ν₁: ", rpad(ele₁[6], 24), "ν₀:", ele₀[6])
+    end
+    failed, ele₁, ele₀
+end
+
+function run_tests(rngs...)
+    iter = product(rngs...)
+    total = length(iter)
+    println("Testing $total combinations with $procs processes.")
+
+    results = pmap(x -> keplerian_roundtrip(x, μ; abort=abort), iter)
+    failed = reduce(+, map(first, results))
+    passed = total - failed
+    percent_failed = round(Int, failed / total * 100.0)
+    percent_passed = round(Int, passed / total * 100.0)
+    println("Passed $passed ($percent_passed%), failed $failed ($percent_failed%).")
+end
+
+println("Elliptic Orbits")
+println("===============")
 
 a_rng = range(7e6, 9e8, length=n)
 e_rng = range(0.001, 0.999, length=n)
@@ -11,53 +66,20 @@ i_rng = range(0.001, 0.999π, length=n)
 Ω_rng = range(0.001, 1.999π, length=n)
 ω_rng = range(0.001, 1.999π, length=n)
 ν_rng = range(-π, π, length=n)
-iter = collect(product(a_rng, e_rng, i_rng, Ω_rng, ω_rng, ν_rng))
-println("Testing $(length(iter)) combinations with $(nthreads()) threads.")
-#= @threads for i in eachindex(iter) =#
-#=     ele₀ = iter[i] =#
-#=     a, e, i, Ω, ω, ν = ele₀ =#
-#=     ele₁ = keplerian(cartesian(a, e, i, Ω, ω, ν, μ)..., μ) =#
-#=     if !(isapprox(ele₁[1], ele₀[1], rtol=1.0) && =#
-#=          isapprox(ele₁[2], ele₀[2], atol=sqrt(eps())) && =#
-#=          isapprox(ele₁[3], ele₀[3], atol=sqrt(eps())) && =#
-#=          isapprox(ele₁[4], ele₀[4], atol=sqrt(eps())) && =#
-#=          isapprox(ele₁[5], ele₀[5], atol=sqrt(eps())) && =#
-#=          isapprox(abs(ele₁[6]), abs(ele₀[6]), atol=sqrt(eps()))) =#
-#=         println("a: ", ele₁[1], " ", ele₀[1]) =#
-#=         println("e: ", ele₁[2], " ", ele₀[2]) =#
-#=         println("i: ", ele₁[3], " ", ele₀[3]) =#
-#=         println("Ω: ", ele₁[4], " ", ele₀[4]) =#
-#=         println("ω: ", ele₁[5], " ", ele₀[5]) =#
-#=         println("ν: ", ele₁[6], " ", ele₀[6]) =#
-#=         break =#
-#=     end =#
-#= end =#
+
+t = @elapsed run_tests(a_rng, e_rng, i_rng, Ω_rng, ω_rng, ν_rng)
+println("In $t seconds.\n")
+
+println("Hyperbolic Orbits")
+println("=================")
 
 a_rng = range(-2e6, -9e8, length=n)
 e_rng = range(1.001, 5, length=n)
 i_rng = range(0.001, 0.999π, length=n)
 Ω_rng = range(0.001, 1.999π, length=n)
 ω_rng = range(0.001, 1.999π, length=n)
-ν_rng = range(-π, π, length=n)
-iter = collect(product(a_rng, e_rng, i_rng, Ω_rng, ω_rng, ν_rng))
-println("Testing $(length(iter)) combinations with $(nthreads()) threads.")
-for i in eachindex(iter)
-    ele₀ = iter[i]
-    a, e, i, Ω, ω, ν = ele₀
-    ele₁ = keplerian(cartesian(a, e, i, Ω, ω, ν, μ)..., μ)
-    #= ele₁ = AstroBase.TwoBody.keplerian_orekit(AstroBase.TwoBody.cartesian_orekit(a, e, i, Ω, ω, ν, μ)..., μ) =#
-    if !(isapprox(ele₁[1], ele₀[1], rtol=1.0) &&
-         isapprox(ele₁[2], ele₀[2], atol=sqrt(eps())) &&
-         isapprox(ele₁[3], ele₀[3], atol=sqrt(eps())) &&
-         isapprox(ele₁[4], ele₀[4], atol=sqrt(eps())) &&
-         isapprox(ele₁[5], ele₀[5], atol=sqrt(eps())) &&
-         isapprox(abs(ele₁[6]), abs(ele₀[6]), atol=sqrt(eps())))
-        Core.println("a₁: ", ele₁[1]," a₀: ", ele₀[1])
-        Core.println("e₁: ", ele₁[2]," e₀: ", ele₀[2])
-        Core.println("i₁: ", ele₁[3]," i₀: ", ele₀[3])
-        Core.println("Ω₁: ", ele₁[4]," Ω₀: ", ele₀[4])
-        Core.println("ω₁: ", ele₁[5]," ω₀: ", ele₀[5])
-        Core.println("ν₁: ", ele₁[6]," ν₀: ", ele₀[6])
-        break
-    end
-end
+ν_rng = range(-0.999π, 0.999π, length=n)
+
+t = @elapsed run_tests(a_rng, e_rng, i_rng, Ω_rng, ω_rng, ν_rng)
+println("In $t seconds.\n")
+
