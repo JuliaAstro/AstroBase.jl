bb5aaa67cae139dae487ef7fb93e8118a844c099
Helge Eichhorn <git@helgeeichhorn.de>
2020-01-07 17:23:25 +0100

    Add new Trajectory draft

--- src/coords/trajectories.jl
+++ src/coords/trajectories.jl
@@ -1,19 +1,62 @@
-using AstroTime: Period
+using AstroTime: Period, Epoch, TimeScale
 using SmoothingSplines
 using LinearAlgebra
 
 import Base: getindex, lastindex, show
 import AstroBase: state
 
-export Trajectory, initial, final, state, events, times,
+export TypedTrajectory, Trajectory, initial, final, state, events, times,
     LogEntry, count_id, id, epoch, detector
 
-struct TypedTrajectory{T<:NamedTuple}
-    series::T
-    names::Vector{Symbol}
+abstract type AbstractTrajectory{Scale, Frame, Body} end
+
+scale(s::AbstractTrajectory{Scale}) where {Scale} = Scale
+frame(s::AbstractTrajectory{_S, Frame}) where {_S, Frame} = Frame
+body(s::AbstractTrajectory{_S, _F, Body}) where {_S, _F, Body} = Body
+
+function transpose_aoa(input::Vector{Vector{T}}) where T
+    n = length(input[1])
+    output = Vector{Vector{T}}(undef, n)
+    matrix = hcat(input...)
+    for i = 1:n
+        output[i] = matrix[i, :]
+    end
+    return output
 end
 
-function TypedTrajectory(data::Matrix; names=Symbol[])
+struct TypedTrajectory{Scale, Frame, Body, TType, T<:NamedTuple} <: AbstractTrajectory{Scale, Frame, Body}
+    series::T
+    function TypedTrajectory(epoch::Epoch{Scale, TType}, time, input::Vector{Vector{T}};
+                             frame::Frame=icrf,
+                             body::Body=earth,
+                             names::Vector{Symbol}=Symbol[]) where {Scale, Frame, Body, TType, T}
+        data = transpose_aoa(input)
+        if isempty(names)
+            if length(data) == 3
+                names = [:x, :y, :z]
+            elseif length(data) == 6
+                names = [:x, :y, :z, :vx, :vy, :vz]
+            else
+                throw(ArgumentError("Explicit `names` need to provided if `data` does not have either 3 or 6 elements."))
+            end
+        end
+        series = (; zip(names, [TimeSeries(epoch, time, d) for d in data])...)
+        new{Scale::TimeScale, frame::AbstractFrame, body::CelestialBody, TType, typeof(series)}(series)
+    end
+end
+
+function (tra::TypedTrajectory)(t)
+    typ = promote_type(eltype.(values(tra.series))...)
+    out = Vector{typ}(undef, length(tra.series))
+    for (i, ts) in enumerate(tra.series)
+        out[i] = ts(t)
+    end
+    return out
+end
+
+function keplerian(tra::TypedTrajectory, t)
+    rv = tra(t)[1:6]
+    return keplerian(rv[1:3], rv[4:6], grav_param(body(tra)))
 end
 
 struct LogEntry
--- test/coords.jl
+++ test/coords.jl
@@ -1,66 +1,80 @@
 using AstroBase
+using LinearAlgebra: norm
+using OrdinaryDiffEq: ODEProblem, solve, Vern9
 using Test
 
-@testset "States" begin
-    r = [
-    6068279.27,
-    -1692843.94,
-    -2516619.18,
+function rhs(du, u, p, t)
+    du[1:3] = u[4:6]
+    r3 = norm(u[1:3])^3
+    du[4:6] = -p .* u[1:3] ./ r3
+end
+
+r = [
+        6068279.27,
+        -1692843.94,
+        -2516619.18,
     ]/1000
 
-    v = [
-    -660.415582,
-    5495.938726,
-    -5303.093233,
+v = [
+        -660.415582,
+        5495.938726,
+        -5303.093233,
     ]/1000
 
-    rv = [r; v]
+rv = [r; v]
 
-    eph = VSOP87()
+ep = UTCEpoch("2016-05-30T12:00:00.000")
 
-    ep = UTCEpoch("2016-05-30T12:00:00.000")
-    tdb = TDBEpoch(ep)
+ele = keplerian(r, v, grav_param(earth))
+t1 = period(ele[1], grav_param(earth))
 
-    iss = State(ep, r, v)
-    rot = Rotation(icrf, iau_earth, ep)
-    pos_mars, vel_mars = state(eph, ep, earth, mars)
-    r_mars = r .- pos_mars
-    v_mars = v .- vel_mars
-    mars_rot = State(tdb, r, v, frame=iau_mars, body=mars)
+@testset "Coords" begin
+    @testset "States" begin
+        eph = VSOP87()
 
-    @testset "Operators" begin
-        @test iss == State(ep, r, v, frame=icrf, body=earth)
-        @test iss ≈ State(ep, r .+ eps(), v .+ eps(), frame=icrf, body=earth)
-    end
-    @testset "Change frame" begin
-        @test State(iss, eph, frame=iau_earth) ≈ State(ep, rot(r, v)..., frame=iau_earth)
-        @test iss ≈ State(State(iss, eph, frame=iau_earth), eph, frame=icrf)
-    end
-    @testset "Change time scale" begin
-        @test State(iss, eph, scale=TDB) == State(tdb, r, v)
-    end
-    @testset "Change central body" begin
-        @test State(iss, eph, body=mars) ==
-            State(ep, r_mars, v_mars, frame=icrf, body=mars)
-        @test State(State(mars_rot, eph, body=earth), eph, body=mars) ≈ mars_rot
-    end
-    @testset "Change scale and body" begin
-        @test State(iss, eph, scale=TDB, body=mars) ==
-            State(tdb, r_mars, v_mars, frame=icrf, body=mars)
-    end
-    @testset "Change frame and body" begin
-        @test State(iss, eph, frame=iau_earth, body=mars) ≈
-            State(ep, rot(r_mars, v_mars)..., frame=iau_earth, body=mars)
-        @test State(State(mars_rot, eph, frame=itrf, body=earth), eph, frame=iau_mars,
-            body=mars) ≈ mars_rot
-    end
-    @testset "Change frame and scale" begin
-        @test State(iss, eph, frame=iau_earth, scale=TDB) ≈
-            State(tdb, rot(r, v)..., frame=iau_earth)
-    end
-    @testset "Change scale, frame, and body" begin
-        @test State(State(mars_rot, eph, frame=itrf, scale=UT1, body=earth), eph,
-            frame=iau_mars, scale=TDB, body=mars) ≈ mars_rot
+        tdb = TDBEpoch(ep)
+
+        iss = State(ep, r, v)
+        rot = Rotation(icrf, iau_earth, ep)
+        pos_mars, vel_mars = state(eph, ep, earth, mars)
+        r_mars = r .- pos_mars
+        v_mars = v .- vel_mars
+        mars_rot = State(tdb, r, v, frame=iau_mars, body=mars)
+
+        @testset "Operators" begin
+            @test iss == State(ep, r, v, frame=icrf, body=earth)
+            @test iss ≈ State(ep, r .+ eps(), v .+ eps(), frame=icrf, body=earth)
+        end
+        @testset "Change frame" begin
+            @test State(iss, eph, frame=iau_earth) ≈ State(ep, rot(r, v)..., frame=iau_earth)
+            @test iss ≈ State(State(iss, eph, frame=iau_earth), eph, frame=icrf)
+        end
+        @testset "Change time scale" begin
+            @test State(iss, eph, scale=TDB) == State(tdb, r, v)
+        end
+        @testset "Change central body" begin
+            @test State(iss, eph, body=mars) ==
+                State(ep, r_mars, v_mars, frame=icrf, body=mars)
+            @test State(State(mars_rot, eph, body=earth), eph, body=mars) ≈ mars_rot
+        end
+        @testset "Change scale and body" begin
+            @test State(iss, eph, scale=TDB, body=mars) ==
+                State(tdb, r_mars, v_mars, frame=icrf, body=mars)
+        end
+        @testset "Change frame and body" begin
+            @test State(iss, eph, frame=iau_earth, body=mars) ≈
+                State(ep, rot(r_mars, v_mars)..., frame=iau_earth, body=mars)
+            @test State(State(mars_rot, eph, frame=itrf, body=earth), eph, frame=iau_mars,
+                body=mars) ≈ mars_rot
+        end
+        @testset "Change frame and scale" begin
+            @test State(iss, eph, frame=iau_earth, scale=TDB) ≈
+                State(tdb, rot(r, v)..., frame=iau_earth)
+        end
+        @testset "Change scale, frame, and body" begin
+            @test State(State(mars_rot, eph, frame=itrf, scale=UT1, body=earth), eph,
+                frame=iau_mars, scale=TDB, body=mars) ≈ mars_rot
+        end
     end
     @testset "Time Series" begin
         t = 1seconds:4seconds
@@ -73,45 +87,24 @@ using Test
         @test ts(ep + 1.5seconds) ≈ 1.5
     end
     @testset "Trajectories" begin
-        v_pos = [
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-        ]
-        v_posvel = [
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-        ]
-        v_posvelmass = [
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-            [1.0, 2.0, 3.0],
-        ]
-        m_pos = [
-            1.0 1.0 1.0;
-            2.0 2.0 2.0;
-            3.0 3.0 3.0;
-        ]
-        m_posvel = [
-            1.0 1.0 1.0 1.0 1.0 1.0;
-            2.0 2.0 2.0 2.0 2.0 2.0;
-            3.0 3.0 3.0 3.0 3.0 3.0;
-        ]
-        m_posvelmass = [
-            1.0 2.0 3.0;
-            1.0 2.0 3.0;
-            1.0 2.0 3.0;
-            1.0 2.0 3.0;
-            1.0 2.0 3.0;
-            1.0 2.0 3.0;
-        ]
+        @testset "Transpose - Array of Arrays" begin
+            a = [[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]]
+            b = [[1.0, 1.0], [2.0, 2.0], [3.0, 3.0]]
+            @test AstroBase.Coords.transpose_aoa(a) == b
+        end
+        @testset "Propagation" begin
+            prob = ODEProblem(rhs, rv, (0.0, value(t1)), p=grav_param(earth))
+            sol = solve(prob, Vern9())
+            tra = TypedTrajectory(ep, sol.t * seconds, sol.u)
+            rv0 = tra(0.0seconds)
+            ele0 = keplerian(tra, 0.0seconds)
+            @test rv0 ≈ rv
+            @test collect(ele0) ≈ collect(ele)
+            rv1 = tra(t1)
+            ele1 = keplerian(tra, t1)
+            @test rv1 ≈ rv atol=0.1
+            @test collect(ele1) ≈ collect(ele) atol=0.1
+        end
     end
 end
 
