c5dfad66fcb36aec536b982bfbf9e64c3fdcf7f1
Helge Eichhorn <git@helgeeichhorn.de>
2020-03-30 09:26:03 +0200

    Clean-up header script

--- bin/update_license_headers.jl
+++ bin/update_license_headers.jl
@@ -1,13 +1,14 @@
 using Dates
 
 package = "AstroBase"
+maintainer = "Helge Eichhorn"
 start_year = 2018
 current_year = year(now())
 
 header =
 """
 #
-# Copyright (c) $start_year-$current_year Helge Eichhorn and the $package.jl contributors
+# Copyright (c) $start_year-$current_year $maintainer and the $package.jl contributors
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -17,21 +18,21 @@ header =
 
 for (root, dirs, files) in walkdir("src")
     for file in files
-		splitext(file)[end] != ".jl" && continue
+        splitext(file)[end] != ".jl" && continue
         fp = joinpath(root, file)
-		str = open(f->read(f, String), fp, "r")
-		if !startswith(str, "#\n# Copyright")
-			println("Added header for: ", fp)
-			open(fp, "w") do f
-				write(f, header * str)
-			end
-		else
-			n = length(header)
-			str[1:n] == header && continue
-			println("Updated header for: ", fp)
-			open(fp, "w") do f
-				write(f, header * str[n+1:end])
-			end
-		end
+        str = open(f->read(f, String), fp, "r")
+        if !startswith(str, "#\n# Copyright")
+            println("Added header for: ", fp)
+            open(fp, "w") do f
+                write(f, header * str)
+            end
+        else
+            n = length(header)
+            str[1:n] == header && continue
+            println("Updated header for: ", fp)
+            open(fp, "w") do f
+                write(f, header * str[n+1:end])
+            end
+        end
     end
 end
