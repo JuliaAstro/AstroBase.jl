6e9b7846cbb86dc77c4d3680bee2888d08baac7b
Helge Eichhorn <git@helgeeichhorn.de>
2020-05-04 08:05:35 +0200

    Match dir names with module names

--- src/AstroBase.jl
+++ src/AstroBase.jl
@@ -17,18 +17,18 @@ end
 Base.showerror(io::IO, err::AstroException) = print(io, err.msg)
 
 include(joinpath("Time", "Time.jl"))
-include(joinpath("constants", "Constants.jl"))
-include(joinpath("interfaces", "Interfaces.jl"))
-include(joinpath("util", "Util.jl"))
-include(joinpath("bodies", "Bodies.jl"))
-include(joinpath("earth_attitude", "EarthAttitude.jl"))
-include(joinpath("two_body", "TwoBody.jl"))
-include(joinpath("ephemerides", "Ephemerides.jl"))
-include(joinpath("frames", "Frames.jl"))
-include(joinpath("coords", "Coords.jl"))
+include(joinpath("Constants", "Constants.jl"))
+include(joinpath("Interfaces", "Interfaces.jl"))
+include(joinpath("Util", "Util.jl"))
+include(joinpath("Bodies", "Bodies.jl"))
+include(joinpath("EarthAttitude", "EarthAttitude.jl"))
+include(joinpath("TwoBody", "TwoBody.jl"))
+include(joinpath("Ephemerides", "Ephemerides.jl"))
+include(joinpath("Frames", "Frames.jl"))
+include(joinpath("Coords", "Coords.jl"))
 
-# include(joinpath("n_body", "NBody.jl"))
-# include(joinpath("astrometry", "Astrometry.jl"))
+# include(joinpath("NBody", "NBody.jl"))
+# include(joinpath("Astrometry", "Astrometry.jl"))
 
 @reexport using .Time
 @reexport using .Constants
