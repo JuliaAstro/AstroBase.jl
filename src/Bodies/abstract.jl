838b133f42c8e9ee503db9ded3717c00f85f1587
Helge Eichhorn <git@helgeeichhorn.de>
2020-05-06 15:19:57 +0200

    Cleanup

--- src/Bodies/abstract.jl
+++ src/Bodies/abstract.jl
@@ -5,6 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
+
 """
     CelestialBody
 
@@ -40,9 +41,6 @@ abstract type NaturalSatellite <: CelestialBody end
 Abstract supertype for minor solar system bodies.
 """
 abstract type MinorBody <: CelestialBody end
-
-# TODO: Use three-arg form?
-Base.show(io::IO, body::CelestialBody) = print(io, string(nameof(typeof(body))))
 
 """
     NAIFId
