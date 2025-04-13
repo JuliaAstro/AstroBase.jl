776f7b7202e538df519eff68996604f3b95da7d8
abhro <5664668+abhro@users.noreply.github.com>
2025-04-13 14:08:29 -0400

    Up size threshold for doc output size

--- docs/make.jl
+++ docs/make.jl
@@ -7,25 +7,26 @@ DocMeta.setdocmeta!(AstroBase, :DocTestSetup, setup; recursive = true)
 
 makedocs(
     format = Documenter.HTML(
-	prettyurls = get(ENV, "CI", nothing) == "true",
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        size_threshold = 400 * 1024, # 400 KiB
     ),
     modules = [AstroBase],
     sitename = "AstroBase.jl",
     authors = "Helge Eichhorn and the AstroBase.jl contributors",
     pages = [
-	"Home" => "index.md",
-	"Modules" => [
-	    "Astrometry" => joinpath("modules", "astrometry.md"),
-	    "Celestial Bodies" => joinpath("modules", "bodies.md"),
-	    "Constants" => joinpath("modules", "constants.md"),
-	    "Coordinates" => joinpath("modules", "coords.md"),
-	    "Earth Attitude" => joinpath("modules", "earth_attitude.md"),
-	    "Ephemerides" => joinpath("modules", "ephemerides.md"),
-	    "Reference Frames" => joinpath("modules", "frames.md"),
-	    "Time" => joinpath("modules", "time.md"),
-	    "Two-Body Problem" => joinpath("modules", "two_body.md"),
-	    "Utilities" => joinpath("modules", "util.md"),
-	],
+        "Home" => "index.md",
+        "Modules" => [
+            "Astrometry" => joinpath("modules", "astrometry.md"),
+            "Celestial Bodies" => joinpath("modules", "bodies.md"),
+            "Constants" => joinpath("modules", "constants.md"),
+            "Coordinates" => joinpath("modules", "coords.md"),
+            "Earth Attitude" => joinpath("modules", "earth_attitude.md"),
+            "Ephemerides" => joinpath("modules", "ephemerides.md"),
+            "Reference Frames" => joinpath("modules", "frames.md"),
+            "Time" => joinpath("modules", "time.md"),
+            "Two-Body Problem" => joinpath("modules", "two_body.md"),
+            "Utilities" => joinpath("modules", "util.md"),
+        ],
     ],
 )
 
